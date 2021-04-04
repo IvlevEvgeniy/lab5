d3b8c13830f1450dc0d9ef9441ee5b24bfb13f67
IvlevEvgeniy <79964221+IvlevEvgeniy@users.noreply.github.com>
2021-04-04 10:43:01 +0300

    Add files via upload

---
+++ lab5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+void decoder(char* s, int* cstr, int n) {
+	char str[3];
+	int i;
+	for (i = 0;i < n;i++) {
+		str[i] = s[cstr[i]];
+	}
+	str[n] = '\0';
+	cout << str << endl;
+}
+int main() {
+	char s[] = "qweityuivpasdlghjkezxcvvnm";
+	int cstr[] = { 3,8,13,2,13 };
+	decoder(s, cstr, 5);
+	return 0;
+}
